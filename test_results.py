7c3dff95093ac03a98dc97a391322fb5f6b47877
Yothgewalt <eine.schlacht@gmail.com>
2024-08-07 21:26:50 +0700

    feat: seperated unit testing function and new mechanics to count passed and failures.

--- test_results.py
+++ test_results.py
@@ -1,13 +1,39 @@
-import junitparser
+from junitparser import JUnitXml
+
+def __unit_testing__(filename: str) -> tuple[int, int]:
+    """
+    Counts the total number of passed and failed tests in a JUnit XML file.
+
+    Args:
+        xml_file: The path to the JUnit XML file.
+
+    Returns:
+        The total number of passed tests.
+    """
+    test_suites = JUnitXml.fromfile(filename)
+    
+    total_passed: int = 0
+    for suite in test_suites:
+        for case in suite:
+            if not case.result:
+                total_passed += 1
+    
+    total_failures: int = test_suites.failures
+    
+    return total_passed, total_failures
 
 def main():
-    junit_report = junitparser.JUnitXml.fromfile('report.xml')
+    report_filename: str = "report.xml"
+    total_passed, total_failures = __unit_testing__(filename=report_filename)
     
-    passed = sum(1 for suite in junit_report for case in suite if case.result._elem is None)
-    failed = sum(1 for suite in junit_report for case in suite if case.result and case.result._elem.tag == 'failure')
+    message = f"""
+    Edinhundert Challenges (Test set):
+    Test set for Computer Programming Problems in the "Student Applications Open For The Coding Skills Program"
     
-    print(f'Tests passed: {passed}')
-    print(f'Tests failed: {failed}')
+    🌱 Tests passed: {total_passed}
+    💥 Tests failed: {total_failures}
+    """
+    print(message)
 
 if __name__ == "__main__":
     main()
